f1dc4468ee47aeb6cdf4fa6a28d8d27eb0050e4e
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2019-05-30 19:44:27 +0200

    Set Klara typeface as the default for the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      body: [
+        '"Karla"',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
     extend: {},
   },
   variants: {},
